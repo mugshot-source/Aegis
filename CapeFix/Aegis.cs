7e7ad1cf1a6dba26487ddfba9c6c2b3b33a5670d
mugshot-source <81108539+mugshot-source@users.noreply.github.com>
2021-04-12 21:57:20 -0400

    Add files via upload

---
+++ CapeFix/Aegis.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using JotunnLib;
+using JotunnLib.Entities;
+using JotunnLib.Managers;
+using JotunnLib.Events;
+using System;
+
+namespace Aegis
+{
+    [BepInPlugin("Aegis", "MagicShield", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class Aegis : PrefabConfig
+    {
+        public Aegis() : base("Aegis", "ShieldKnight")
+        {
+
+        }
+
+        public override void Register()
+        {
+
+            ItemDrop item = Prefab.GetComponent<ItemDrop>();
+            item.m_itemData.m_shared.m_name = "Aegis";
+            item.m_itemData.m_shared.m_description = "A powerful shield encrusted with crystal, and imbued with fuling magic.";
+            item.m_itemData.m_shared.m_blockPower = 40;
+            item.m_itemData.m_shared.m_blockPowerPerLevel = 10;
+            item.m_itemData.m_shared.m_timedBlockBonus = 1.1f;
+            item.m_itemData.m_shared.m_equipDuration = 1;
+            item.m_itemData.m_shared.m_weight = 5;
+            item.m_itemData.m_shared.m_maxDurability = 1000;
+            item.m_itemData.m_shared.m_durabilityPerLevel = 100;
+            item.m_itemData.m_shared.m_deflectionForce = 10000;
+            item.m_itemData.m_shared.m_itemType = ItemDrop.ItemData.ItemType.Shield;
+            item.m_itemData.m_shared.m_maxQuality = 1;
+            item.m_itemData.m_shared.m_movementModifier = -0.2f;
+            item.m_itemData.m_shared.m_variants = 0;
+
+            //item.m_itemData.m_shared.m_icons = new Sprite[] { AssetHelper.Icon };
+
+            var shielded = ScriptableObject.CreateInstance<SE_Shielded>();
+            //shielded.m_icon = AssetHelper.Icon;
+
+            /*            var shieldValue = AccessTools.Field(typeof(SE_Shielded), "m_absorbDamage");
+
+                            float shieldHP = 300f;
+
+                            shieldValue.SetValue(shielded, shieldHP);*/
+
+            item.m_itemData.m_shared.m_equipStatusEffect = shielded;
+        }
+    }
+}
---
+++ CapeFix/Mod.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using BepInEx;
+using HarmonyLib;
+using JotunnLib;
+using JotunnLib.Managers;
+using JotunnLib.Entities;
+using BepInEx.Logging;
+
+namespace Aegis
+{
+    [BepInPlugin("Aegis", "MagicShield", "1.0.0")]
+    [BepInDependency(JotunnLib.JotunnLib.ModGuid)]  
+
+    public class Mod : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("Aegis");
+        public static ManualLogSource log;
+        private void Awake()
+        {
+            PrefabManager.Instance.PrefabRegister += RegisterPrefabs;
+            ObjectManager.Instance.ObjectRegister += registerObjects;
+            harmony.PatchAll();
+        }
+
+        private void RegisterPrefabs(object sender, EventArgs e)
+        {
+            //AccessTools.Method(typeof(PrefabManager), "RegisterPrefab", new Type[] { typeof(GameObject), typeof(string) }).Invoke(PrefabManager.Instance, new Object[] {, });
+            PrefabManager.Instance.RegisterPrefab(new Aegis());
+        }
+        private void registerObjects(object sender, EventArgs e)
+        {
+            ObjectManager.Instance.RegisterItem("Aegis");
+
+            ObjectManager.Instance.RegisterRecipe(new RecipeConfig()
+            {
+                Name = "Recipe_Aegis",
+                Item = "Aegis",
+                CraftingStation = "forge",
+                RepairStation = "forge",
+
+                Requirements = new PieceRequirementConfig[]
+                {
+                    new PieceRequirementConfig()
+                    {
+                        Item = "Crystal",
+                        Amount = 5
+                    },
+                    new PieceRequirementConfig()
+                    {
+                        Item = "Silver",
+                        Amount = 30
+                    },
+                     new PieceRequirementConfig()
+                    {
+                        Item = "FineWood",
+                        Amount = 10
+                    },
+                     new PieceRequirementConfig()
+                    {
+                        Item = "GoblinTotem",
+                        Amount = 4
+                    }
+                }
+            });
+            }
+
+
+        static void PlayEffect(string prefabN, Vector3 pos)
+        {
+            GameObject prefab = ZNetScene.instance.GetPrefab(prefabN);
+            if (prefab != null)
+            {
+                UnityEngine.Object.Instantiate<GameObject>(prefab, pos, Quaternion.identity);
+                return;
+            }
+            log.LogWarning("Failed to locate effect " + prefabN + " prefab");
+        }
+
+        [HarmonyPatch(typeof(SE_Shielded), "Setup")]
+        class ShieldPatch
+        {
+            static void Prefix()
+            {
+                Player localPlayer = Player.m_localPlayer;
+                Vector3 vector = localPlayer.transform.position;
+                PlayEffect("fx_guardstone_permitted_add", vector);
+                //PlayEffect("fx_guardstone_permitted_add", vector);
+            }
+        }
+
+        [HarmonyPatch(typeof(SE_Shielded), "OnDamaged")]
+        class DamagePatch
+        {
+            static void Prefix()
+            {
+                Player localPlayer = Player.m_localPlayer;
+                Vector3 vector = localPlayer.transform.position;
+                PlayEffect("fx_GoblinShieldHit", vector);
+                //PlayEffect("fx_guardstone_permitted_removed", vector);
+            }
+        }
+
+/*        [HarmonyPatch(typeof(SE_Shielded), "IsDone")]
+        class BreakPatch
+        {
+            static void Prefix()
+            {
+                Player localPlayer = Player.m_localPlayer;
+                Vector3 vector = localPlayer.transform.position;
+                PlayEffect("fx_guardstone_permitted_remove", vector);
+            }
+        }*/
+    }
+}
